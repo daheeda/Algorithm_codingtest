8a1dfb749461a17b941f66cfe5bd0d969a8f9602
Dahee Choi <91541933+daheeda@users.noreply.github.com>
2022-10-31 20:37:30 +0900

    Update 1463_dahee_1031.py

--- dp/1463_dahee_1031.py
+++ dp/1463_dahee_1031.py
@@ -3,8 +3,8 @@ dp = [x for _ in range(x+1)]
 dp[1]=0
 for index in range(1,x):
     if index*3 <= x :
-        dp[index*3] = min(dp[index*3],dp[index]+1,index)
+        dp[index*3] = min(dp[index*3],dp[index]+1)
     if index*2 <= x :
-        dp[index*2] = min(dp[index*2],dp[index]+1,index)
-    dp[index+1] = min(dp[index+1],dp[index]+1,index)
+        dp[index*2] = min(dp[index*2],dp[index]+1)
+    dp[index+1] = min(dp[index+1],dp[index]+1)
 print(dp[x])

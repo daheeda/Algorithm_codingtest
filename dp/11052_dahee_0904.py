dee5571d650ba2d23bab986209f743095943a53e
Dahee Choi <91541933+daheeda@users.noreply.github.com>
2023-09-04 23:31:30 +0900

    Create 11052_dahee_0904.py

---
+++ dp/11052_dahee_0904.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 구매하려는 카드의 개수
+pay = list(map(int,input().split()))
+
+pay.insert(0,0)
+dp = pay.copy()
+
+for i in range(1,n+1):
+    for j in range(0,i):
+        if i%2 == 0: dp[i] = max(dp[i-j]+dp[j],dp[int(i/2)]*2, dp[i])
+        else :dp[i] = max(dp[i-j]+dp[j], dp[i])
+    
+print(dp[-1])
